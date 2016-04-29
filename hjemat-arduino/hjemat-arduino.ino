9a388ae17af7d6602091858dfcf57988600c93c3
Mads Grau Kristensen <madsgrau@gmail.com>
2016-04-29 14:22:39 +0200

    It works!

--- hjemat-arduino/hjemat-arduino.ino
+++ hjemat-arduino/hjemat-arduino.ino
@@ -1,3 +1,6 @@
+//#include <SoftwareSerial.h>
+//SoftwareSerial mySerial(2, 3);
+
 byte data[4];
 byte id = 0x2;
 
@@ -16,10 +19,11 @@ byte comSet       = 0x4;
 byte comReturn    = 0x5;
 
 void setup() {
-  Serial.begin(9600, SERIAL_8O1);
-  pinMode(ledPin, OUTPUT);
+  Serial.begin(115200);
+  //Serial.begin(115200);
 
   DDRB = DDRB | B00110000;
+  PORTB = PORTB & B11011111;
 }
 
 void loop() {
@@ -27,6 +31,9 @@ void loop() {
     return;
     
   Serial.readBytes(data, 4);
+  /*Serial.print(data[0], BIN);
+  Serial.print(" | ");
+  Serial.println(data[0] >> 3);*/
 
   if (data[0] != 0){
     if (id == 0x0) {
@@ -39,10 +46,12 @@ void loop() {
     }
     else {
       if (data[0] >> 3 == id){
+        //Serial.println("I've been summoned!");
         byte kom = data[0] & B00000111;
         
         if (kom == comSet)
         {
+          //Serial.println("I've been told to do something");
           byte dataType =  data[1];
           int value = data[2];
           value = value << 8;
@@ -54,16 +63,22 @@ void loop() {
         }
         else if (kom == comGet)
         {
+          //Serial.println("I've to tell something");
           byte dataID =  data[1];
 
           returnValue(dataID);
         }
         else if (kom == comPing)
         {
+          //Serial.println("I've have to tell that I'm awake!");
           byte header = createHeader(comBack);
           byte pingback[4] = { header, productID[0], productID[1], productID[2] };
-
+          delay(150);
           tWrite(pingback, 4);
+        }
+        else
+        {
+          //Serial.println("I've do not know that they want :(");
         }
       }
     }
@@ -109,11 +124,12 @@ void handleDataChange(byte dataType, int value) {
 
 void tWrite(byte bytes[], int numBytes)
 {
+  //Serial.println("Writing fine data");
   PORTB = PORTB | B00100000;
-  delay(50);
+  delay(5);
   Serial.write(bytes, numBytes);
 
-  delay(50);
+  delay(5);
   PORTB = PORTB & B11011111;
 }
 
