fd30d38f5e2ac9c2320f1d0e4228019d207e18b9
Mads Grau Kristensen <madsgrau@gmail.com>
2016-05-10 12:46:56 +0200

    Added support for pairing, hopefully

--- HjematDevice/HjematDevice.cpp
+++ HjematDevice/HjematDevice.cpp
@@ -10,16 +10,13 @@
 #define comSet      0x4
 #define comReturn   0x5
 
-byte _message[4];
-byte _messageID;
-byte _messageCommand;
-byte _id = 0x0;
+#define pairComAllow   0x1
+#define pairComRequest 0x2
+#define pairComReturn  0x3
+#define pairComStop    0x4
 
-//byte _handshakeMessage[4] = { B00000101, 0x0, 0x0, 0x10 };
-
-byte _productID[] = { 0x0, 0x0, 0x1 };
-
-HjematDevice::HjematDevice(long productID) {
+HjematDevice::HjematDevice(long productID)
+{
   this->_productID[0] = productID >> 16;
   this->_productID[1] = productID >> 8;
   this->_productID[2] = productID;
@@ -36,16 +33,37 @@ void HjematDevice::loop() {
     
   Serial.readBytes(_message, 4);
 
+  if (_id == 0 && _pairingAllowed) {
+    if (digitalRead(buttonPin) == HIGH) {
+      sendPairRequest();
+      _pairRequestSend = false;
+    }
+  }
+
   if (_message[0] != 0) {           // We got a message
     _messageID = _message[0] >> 3;
     _messageCommand = _message[0] & B00000111;
     
     if (_messageID == _id){      //The message is refering to this device's id
       if (_messageID == 0 ) {           // The message is about pairing
-        // Handle Pairing
+        switch(_messageCommand) {
+          case(pairComAllow):
+            _pairingAllowed = true;
+            break;
+          case(pairComReturn):
+            if (_pairRequestSend) {
+              _id = _message[1];
+            }
+            break;
+          case(pairComStop):
+            _pairingAllowed = false;
+            _pairRequestSend = false;
+            break;
+          default:
+            break;
+        }
       }
       else {                            // The message is a command
-        
         switch(_messageCommand) {
           case(comPing):
             sendPing();
@@ -55,58 +73,25 @@ void HjematDevice::loop() {
             break;
           case(comSet):
             handleDataChange(_message);
+            break;
+          default:
+            break;
         }
       }
     }
   }
-/*
-  if (_data[0] != 0){
-    if (_id == 0x0) {
-      if (_data[0] == B00000011) {
-        tWrite(handshakeMessage, 4);
-      }
-      else if (data[0] == B00000101) {
-        id = data[1];
-      }
-    }
-    else {
-      if (data[0] >> 3 == id){
-        //Serial.println("I've been summoned!");
-        byte kom = data[0] & B00000111;
-        
-        if (kom == comSet)
-        {
-          //Serial.println("I've been told to do something");
-          byte dataType =  data[1];
-          int value = data[2];
-          value = value << 8;
-          value = value | data[3];
-
-          handleDataChange(dataType, value);
-
-          handleReturnValue(dataType);
-        }
-        else if (kom == comGet)
-        {
-          //Serial.println("I've to tell something");
-          byte dataID =  data[1];
-
-          handleReturnValue(dataID);
-        }
-        else if (kom == comPing)
-        {
-          //Serial.println("I've have to tell that I'm awake!");
-          byte header = createHeader(comBack);
-          byte pingback[4] = { header, productID[0], productID[1], productID[2] };
-          delay(150);
-          tWrite(pingback, 4);
-        }
-      }
-    }
-  }*/
 }
 
-void HjematDevice::sendPing() {
+void HjematDevice::sendPairRequest()
+{
+  byte header = createHeader(0, pairComRequest);
+  byte returnMessage[4] = { header, _productID[0], _productID[1], _productID[2] };
+
+  tWrite(returnMessage);
+}
+
+void HjematDevice::sendPing()
+{
   byte header = createHeader(comBack);
   byte pingback[4] = { header, this->_productID[0], this->_productID[1], this->_productID[2] };
   delay(100);
@@ -115,7 +100,12 @@ void HjematDevice::sendPing() {
 
 byte HjematDevice::createHeader(byte command)
 {
-  byte header = this->_id << 3;
+  return createHeader(_id, command);
+}
+
+byte HjematDevice::createHeader(byte id, byte command)
+{
+  byte header = id << 3;
   header = header | command;
   return header;
 }
@@ -132,7 +122,8 @@ void HjematDevice::handleReturnValue(byte dataID)
   tWrite(returnMessage, 4);
 }
 
-void HjematDevice::handleDataChange(byte message[]) {
+void HjematDevice::handleDataChange(byte message[])
+{
   byte dataType =  message[1];
   short value = message[2];
   
--- HjematDevice/HjematDevice.h
+++ HjematDevice/HjematDevice.h
@@ -14,13 +14,21 @@ class HjematDevice
   private:
     // Variables
     byte _productID[3];
-    byte _data[4];
-    byte _id;
+    byte _id = 0;
+
+    byte _message[4];
+    byte _messageID;
+    byte _messageCommand;
+
+    bool _pairingAllowed = false;  
+    bool _pairRequestSend = false;  
   
     byte createHeader(byte command);
+    byte createHeader(byte id, byte command);
     void handleReturnValue(byte dataID);
     void handleDataChange(byte message[]);
     void sendPing();
+    void sendPairRequest();
     
     void tWrite(byte bytes[], int numBytes);
     void tWrite(byte bytes[]);
